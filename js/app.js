58c09c5e340b5d24623e48c0fccbb2fbf2462ed2
seb <niveletsebastien84@gmail.com>
2025-04-15 13:54:44 +0200

    modif laurent

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let btnCloseMainNav = document.querySelector('.closeMainNav');
-    let btnOpenMainNav = document.querySelector('.openMainNav');
+    let btnCloseMainNav = document.querySelector('.closeMainNav'),
+    btnOpenMainNav = document.querySelector('.openMainNav'),
+    btnShare = document.querySelector('.btn-share'),
+    btnShareCloseModal = document.querySelector('.btn-close');
 
+    // Gestion du menu
     btnCloseMainNav.addEventListener('click', function() {
         document.body.classList.remove('has-menu-opened');
     });
 
     btnOpenMainNav.addEventListener('click', function() {
         document.body.classList.add('has-menu-opened');
+    });
+
+
+    // Gestion de la fenêtre de partage
+    btnShare.addEventListener('click', function() {
+        document.body.classList.add('has-share-open', 'has-overlay');
+    });
+
+    btnShareCloseModal.addEventListener('click', function() {
+        document.body.classList.remove('has-share-open', 'has-overlay');
+
     });
 });
